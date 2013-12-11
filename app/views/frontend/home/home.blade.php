a83d5acbb2424e02cb24df1b905bd7a3f8ec4dd2
Sophy Prak <sophy.prak@gmail.com>
2013-12-11 19:37:29 +0700

    Package for npm

--- app/views/frontend/home/home.blade.php
+++ app/views/frontend/home/home.blade.php
@@ -2,3 +2,53 @@
 @section('title')
 	Oh Khmer
 @stop
+@section('content')
+<div class="main-site">
+	<div class="row">
+		<div class="col-md-8">
+			<div class="photo">
+				<div class="action-bar">
+					@include('frontend.elements.actionbar')
+				</div>
+				<div class="content-img">
+					<div class="title-img">
+						<h2><a href="#">Lorem is sume image</a></h2>
+					</div>
+					<div class="img">
+						<img src="{{asset('upload/funny/551401_667722579928108_810506891_n.jpg')}}">
+					</div>
+				</div>
+			</div>
+			<div class="photo">
+				<div class="action-bar">
+					@include('frontend.elements.actionbar')
+				</div>
+				<div class="content-img">
+					<div class="title-img">
+						<h2><a href="#">Lorem is sume image</a></h2>
+					</div>
+					<div class="img">
+						<img src="{{asset('upload/funny/1450913_271623916324943_89742070_n.jpg')}}">
+					</div>
+				</div>
+			</div>
+			<div class="photo">
+				<div class="action-bar">
+					@include('frontend.elements.actionbar')
+				</div>
+				<div class="content-img">
+					<div class="title-img">
+						<h2><a href="#">Lorem is sume image</a></h2>
+					</div>
+					<div class="img">
+						<img src="{{asset('upload/funny/1450048_10202451970041267_60408951_n.jpg')}}">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			Sidebar
+		</div>
+	</div>
+</div>
+@stop
